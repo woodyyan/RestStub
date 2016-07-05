9f73e1ddbe311342a1098da4f66a7e0c4ebb86df
easystudio <easystudio@outlook.com>
2016-07-05 17:28:53 +0800

    添加了get和post一个user。

--- src/main/java/com/github/reststub/StubController.java
+++ src/main/java/com/github/reststub/StubController.java
@@ -1,20 +1,37 @@
 package com.github.reststub;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
-import java.util.concurrent.atomic.AtomicLong;
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 public class StubController {
 
-    private static final String template = "Hello, %s!";
-    private final AtomicLong counter = new AtomicLong();
+    private List<User> users;
 
-    @RequestMapping("/greeting")
-    public String greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
-        return counter.incrementAndGet() +
-                String.format(template, name);
+    public StubController(){
+        users = new ArrayList<>();
+        User user = new User();
+        user.setId(1);
+        user.setName("Woody");
+        user.setAge(20);
+        users.add(user);
+    }
+
+    @RequestMapping("/allusers")
+    public List<User> getAllUsers() {
+        return users;
+    }
+
+    @RequestMapping("/users")
+    public User findUserByName(@RequestParam(value = "name", defaultValue = "World") String name) {
+        return users.get(0);
+    }
+
+    @RequestMapping(method = RequestMethod.POST, value = "/users")
+    public String create(@RequestBody User user) {
+        users.add(user);
+        return "201";
     }
 }
