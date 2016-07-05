20ad638706b7f2f558506254d8ff8dbe268fcadc
easystudio <easystudio@outlook.com>
2016-07-05 17:14:19 +0800

    成功运行项目了。

---
+++ src/main/java/com/github/reststub/StubController.java
@@ -0,0 +1,20 @@
+package com.github.reststub;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+@RestController
+public class StubController {
+
+    private static final String template = "Hello, %s!";
+    private final AtomicLong counter = new AtomicLong();
+
+    @RequestMapping("/greeting")
+    public String greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
+        return counter.incrementAndGet() +
+                String.format(template, name);
+    }
+}
